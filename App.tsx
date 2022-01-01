ec60723c6c64ffedc35f98b28611cefec71cf54a
Melker Larsson <melker.programmering@gmail.com>
2022-01-01 17:37:54 +0100

    Google Maps setup

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import HomeScreen from './src/screens/HomeScreen';
 export default function App() {
   return (
     <NavigationContainer>
-      <RootStack.Navigator>
+      <RootStack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <RootStack.Screen name="Home" component={HomeScreen} />
         <RootStack.Screen name="Onboarding" component={OnboardingScreen} />
       </RootStack.Navigator>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
-
-interface HomeProps {
+const { width, height } = Dimensions.get('window');
+interface HomeScreenProps {
 
 }
 
-const HomeScreen: React.FunctionComponent<HomeProps> = () => {
+const HomeScreen = ({}: HomeScreenProps) => {
 
   return (
-    <View></View>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: width,
+    height: height,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default HomeScreen;
